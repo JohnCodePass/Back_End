7cad7c9edda337d1ab1ac8bb88c2d7a79c7a22df
huegogh <huegogh42@gmail.com>
2021-04-12 10:06:25 -0700

    update route model

--- Models/Route.cs
+++ Models/Route.cs
@@ -2,11 +2,11 @@ namespace Back_End.Models
 {
     public class Route
     {
-        public int RouteNumber{get;set;}
-        public string TypeOfDay{get;set;}
-        public int NumberOfStops{get;set;}
-        public string Direction{get;set;}
-        public Stop[] Stops{get;set;}
+        public int RouteNumber {get;set;}
+        public string TypeOfDay {get;set;}
+        public string Direction {get;set;}
+        public string Stops {get;set;}
+        public int trip {get;set;}
 
     }
 }
