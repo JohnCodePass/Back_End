722e7c3b70c23c8523ed6b7871e2bc0259305c73
PlasmaWreck <31967552+PlasmaWreck@users.noreply.github.com>
2021-04-13 12:19:10 -0700

    New stuff

--- Context/Data.cs
+++ Context/Data.cs
@@ -9,41 +9,32 @@ namespace Back_End.Context
         public DbSet<UserInfo> UserInfo { get; set; }
         public DbSet<Route> Route { get; set; }
         public DbSet<Path> Path { get; set; }
-
-
         public Data(DbContextOptions<Data> options) : base(options)
         {
 
         }
-
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
             SeedData(builder);
         }
-
         private void SeedData(ModelBuilder builder)
         {
             var userFixedData = new List<UserInfo>
             {
                 new UserInfo(1, "huegogh", "password"),
             };
-
             var routeFixedData = new List<Route>
             {
                 new Route(1, 40, "BART EXPRESS"),
             };
-
             var pathFixedData = new List<Path>
             {
-                new Path(1, 40, "weekday", "North", "['stop 1']", "['9:55am']"),
+                new Path(1, "local", 40, "weekday", "North", "['stop 1']", "['9:55am']"),
             };
-
             builder.Entity<UserInfo>().HasData(userFixedData);
             builder.Entity<Route>().HasData(routeFixedData);
             builder.Entity<Path>().HasData(pathFixedData);
         }
-
-
     }
 }
--- Models/Path.cs
+++ Models/Path.cs
@@ -3,6 +3,7 @@ namespace Back_End.Models
     public class Path
     {
         public int id { get; set; }
+        public string typeOfRoute { get; set; }
         public int routeNumber { get; set; }
         public string typeOfDay { get; set; }
         public string direction { get; set; }
@@ -11,10 +12,11 @@ namespace Back_End.Models
 
         public Path(){}
 
-        public Path( int id, int routeNumber, string typeOfDay, string direction, string stopNames, string tripTimes)
+        public Path( int id, string typeOfRoute ,int routeNumber, string typeOfDay, string direction, string stopNames, string tripTimes)
         {
             this.id = id;
             this.routeNumber = routeNumber;
+            this.typeOfRoute = typeOfRoute;
             this.typeOfDay = typeOfDay;
             this.direction = direction;
             this.tripTimes = tripTimes;
